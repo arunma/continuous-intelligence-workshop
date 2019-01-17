3013fee09ed2825d7dcb4d8d654419199f08536d
Arun Manivannan <arun@arunma.com>
2019-01-17 16:37:23 +0800

    Adding tenant config

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ set -xe
 
 IMAGE_VERSION=${GO_PIPELINE_LABEL:-latest}
 # PROJECT_ID=${GCLOUD_PROJECT_ID:-continuous-intelligence}
-# TENANT_NAMESPACE=${TENANT:-admin}
+TENANT_NAMESPACE=${TENANT:-admin}
 # echo "Deploying image version: $IMAGE_VERSION"
 
 # cat kubernetes/web.yml | sed "s/\\\$tenant\\\$/$TENANT_NAMESPACE/" | sed "s/\(image: \).*$/\1eu.gcr.io\/$PROJECT_ID\/ci-workshop-app:$TENANT_NAMESPACE.$IMAGE_VERSION/" | kubectl apply -f -
