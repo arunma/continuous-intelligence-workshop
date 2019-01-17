1a34a3b816b7ece7b30ed99bc328a403c5259020
Arun Manivannan <arun@arunma.com>
2019-01-17 16:34:20 +0800

    Setting local

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 set -xe
 
 IMAGE_VERSION=${GO_PIPELINE_LABEL:-latest}
-PROJECT_ID=${GCLOUD_PROJECT_ID:-ai-sg-workshop}
+PROJECT_ID=${GCLOUD_PROJECT_ID:-continuous-intelligence}
 TENANT_NAMESPACE=${TENANT:-admin}
 echo "Deploying image version: $IMAGE_VERSION"
 
-cat kubernetes/web.yml | sed "s/\\\$tenant\\\$/$TENANT_NAMESPACE/" | sed "s/\(image: \).*$/\1us.gcr.io\/$PROJECT_ID\/ci-workshop-app:$TENANT_NAMESPACE.$IMAGE_VERSION/" | kubectl apply -f -
+cat kubernetes/web.yml | sed "s/\\\$tenant\\\$/$TENANT_NAMESPACE/" | sed "s/\(image: \).*$/\1eu.gcr.io\/$PROJECT_ID\/ci-workshop-app:$TENANT_NAMESPACE.$IMAGE_VERSION/" | kubectl apply -f -
 
 external_ip=""
 while [ -z $external_ip ]; do
@@ -14,4 +14,4 @@ while [ -z $external_ip ]; do
   external_ip=$(kubectl get svc ci-workshop-web --namespace=$TENANT_NAMESPACE --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
   [ -z "$external_ip" ] && sleep 10
 done
-echo "End point ready: http://$external_ip:5005"
+echo "End point ready: http://$external_ip:5005"``
