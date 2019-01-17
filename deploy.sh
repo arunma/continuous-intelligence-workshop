3207ffc39e3c22515462744c1fc4c24e0a39fd7e
Arun Manivannan <arun@arunma.com>
2019-01-17 16:34:20 +0800

    Setting local

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,18 @@
 set -xe
 
 IMAGE_VERSION=${GO_PIPELINE_LABEL:-latest}
-PROJECT_ID=${GCLOUD_PROJECT_ID:-continuous-intelligence}
-TENANT_NAMESPACE=${TENANT:-admin}
-echo "Deploying image version: $IMAGE_VERSION"
+# PROJECT_ID=${GCLOUD_PROJECT_ID:-continuous-intelligence}
+# TENANT_NAMESPACE=${TENANT:-admin}
+# echo "Deploying image version: $IMAGE_VERSION"
 
-cat kubernetes/web.yml | sed "s/\\\$tenant\\\$/$TENANT_NAMESPACE/" | sed "s/\(image: \).*$/\1eu.gcr.io\/$PROJECT_ID\/ci-workshop-app:$TENANT_NAMESPACE.$IMAGE_VERSION/" | kubectl apply -f -
+# cat kubernetes/web.yml | sed "s/\\\$tenant\\\$/$TENANT_NAMESPACE/" | sed "s/\(image: \).*$/\1eu.gcr.io\/$PROJECT_ID\/ci-workshop-app:$TENANT_NAMESPACE.$IMAGE_VERSION/" | kubectl apply -f -
 
-external_ip=""
-while [ -z $external_ip ]; do
-  echo "Waiting for end point..."
-  external_ip=$(kubectl get svc ci-workshop-web --namespace=$TENANT_NAMESPACE --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
-  [ -z "$external_ip" ] && sleep 10
-done
-echo "End point ready: http://$external_ip:5005"
+# external_ip=""
+# while [ -z $external_ip ]; do
+#   echo "Waiting for end point..."
+#   external_ip=$(kubectl get svc ci-workshop-web --namespace=$TENANT_NAMESPACE --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
+#   [ -z "$external_ip" ] && sleep 10
+# done
+# echo "End point ready: http://$external_ip:5005"
+
+docker run -d -p 5005:5005 ci-workshop:$TENANT_NAMESPACE.$IMAGE_VERSION
