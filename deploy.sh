4186a2f8d10e20c6847466d165c4017b239cbd4c
Arun Manivannan <arun@arunma.com>
2019-01-24 22:24:30 +0800

    Enabling GKE

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,11 @@
 set -xe
 
 IMAGE_VERSION=${GO_PIPELINE_LABEL:-latest}
+PROJECT_ID=${GCLOUD_PROJECT_ID:-continuous-intelligence}
 TENANT_NAMESPACE=${TENANT:-admin}
 echo "Deploying image version: $IMAGE_VERSION"
 
-cat kubernetes/web-mini.yml | sed "s/\\\$tenant\\\$/$TENANT_NAMESPACE/" | sed "s/\(image: \).*$/\1$DOCKER_USER/ci-workshop-app:$TENANT_NAMESPACE.$IMAGE_VERSION/" | kubectl apply -f -
+cat kubernetes/web.yml | sed "s/\\\$tenant\\\$/$TENANT_NAMESPACE/" | sed "s/\(image: \).*$/\1eu.gcr.io\/$PROJECT_ID/ci-workshop-app:$TENANT_NAMESPACE.$IMAGE_VERSION/" | kubectl apply -f -
 
 external_ip=""
 while [ -z $external_ip ]; do
