e416725739a5e767a440af8d499e7829aeb92361
Arun Manivannan <arun@arunma.com>
2019-01-24 22:17:39 +0800

    Enabling monitoring

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ echo "Deploying image version: $IMAGE_VERSION"
 
 cat kubernetes/web-mini.yml | sed "s/\\\$tenant\\\$/$TENANT_NAMESPACE/" | sed "s/\(image: \).*$/\1$DOCKER_USER/ci-workshop-app:$TENANT_NAMESPACE.$IMAGE_VERSION/" | kubectl apply -f -
 
-#external_ip=""
-#while [ -z $external_ip ]; do
-#  echo "Waiting for end point..."
-#  external_ip=$(kubectl get svc ci-workshop-web --namespace=$TENANT_NAMESPACE --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
-#  [ -z "$external_ip" ] && sleep 10
-#done
-#echo "End point ready: http://$external_ip:5005"
+external_ip=""
+while [ -z $external_ip ]; do
+  echo "Waiting for end point..."
+  external_ip=$(kubectl get svc ci-workshop-web --namespace=$TENANT_NAMESPACE --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
+  [ -z "$external_ip" ] && sleep 10
+done
+echo "End point ready: http://$external_ip:5005"
