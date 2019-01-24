b1caf30d4205e853754da7db199772d306b333c1
Arun Manivannan <arun@arunma.com>
2019-01-24 22:17:09 +0800

    Fixing typo in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ IMAGE_VERSION=${GO_PIPELINE_LABEL:-latest}
 TENANT_NAMESPACE=${TENANT:-admin}
 echo "Deploying image version: $IMAGE_VERSION"
 
-cat kubernetes/web-mini.yml | sed "s/\\\$tenant\\\$/$TENANT_NAMESPACE/" | sed "s/\(image: \).*$/\1$DOCKER_USER\/ci-workshop-app:$TENANT_NAMESPACE.$IMAGE_VERSION/" | kubectl apply -f -
+cat kubernetes/web-mini.yml | sed "s/\\\$tenant\\\$/$TENANT_NAMESPACE/" | sed "s/\(image: \).*$/\1$DOCKER_USER/ci-workshop-app:$TENANT_NAMESPACE.$IMAGE_VERSION/" | kubectl apply -f -
 
 #external_ip=""
 #while [ -z $external_ip ]; do
